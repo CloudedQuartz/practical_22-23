ea21d5a01101eddd0bd991e04731fbbaf92b41da
CloudedQuartz <ravenklawasd@gmail.com>
2022-08-21 17:52:12 +0530

    mysqlpython1: reformat
    - spaces -> tabs
    - remove mysql username/password
    - capitalize SQL commands
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

---
+++ mysqlpython1.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+MYSQLUSR = ''
+MYSQLPASS = ''
+mydb = mysql.connector.connect(host="localhost", user=MYSQLUSR, passwd=MYSQLPASS)
+cursorObject = mydb.cursor()
+cursorObject.execute("use mydb")
+
+
+def addrecords():
+	cursorObject.execute("USE mydb")
+	sql = "INSERT INTO ITEM VALUES(%s,%s,%s)"
+	x = input("Enter itemcode: ")
+	y = input("Enter Itemname: ")
+	z = float(input("Enter Price: "))
+	val = (x, y, z)
+
+	cursorObject.execute(sql, val)
+	mydb.commit()
+
+
+def showrec():
+	Query = "SELECT * FROM item"
+	cursorObject.execute(Query)
+	records = cursorObject.fetchall()
+	for row in records:
+		print(row)
+
+
+def search():
+	Q = "SELECT * FROM item WHERE Itemcode = %s"
+	s = input("Enter the item code: ")
+	val = (s,)
+	cursorObject.execute(Q, val)
+	f = cursorObject.fetchall()
+	for rows in f:
+		print(rows)
+while True:
+	l = input(""" A: Add a record
+	B: Show all records
+	C: Search a record by itemcode
+	""")
+	match l.lower():
+		case 'a':
+			addrecords()
+		case 'b':
+			showrec()
+		case 'c':
+			search()
