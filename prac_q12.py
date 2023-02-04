750743eabd50b059d8e9027532d77a40ddc54b2e
CloudedQuartz <ayan.pande06@gmail.com>
2023-02-04 17:25:07 +0530

    prac_q{12,33}: tab -> spaces
    - maintain format in other text editors
    
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- prac_q12.py
+++ prac_q12.py
@@ -1,20 +1,20 @@
 Stack = []
 
 def PopStack():
-    return Stack.pop()
+	return Stack.pop()
 
 def AddToStack(num):
-    tmpStack = []
-    if len(Stack) > 0:
-        while Stack[-1] > num:
-            tmpStack.append(PopStack())
-    Stack.append(num)
-    for i in tmpStack[::-1]:
-        Stack.append(i)
+	tmpStack = []
+	if len(Stack) > 0:
+		while Stack[-1] > num:
+			tmpStack.append(PopStack())
+	Stack.append(num)
+	for i in tmpStack[::-1]:
+		Stack.append(i)
 
 def DisplayStack():
-    for i in Stack[::-1]:
-        print(i)
+	for i in Stack[::-1]:
+		print(i)
 
 print('Pick an action')
 print('\t1. Display')
@@ -22,11 +22,11 @@ print('\t2. Add')
 print('\t3. Exit')
 
 while True:
-    match int(input('Choice : ')):
-        case 1:
-            DisplayStack()
-        case 2:
-            num = int(input('Number to add : '))
-            AddToStack(num)
-        case 3:
-            break
+	match int(input('Choice : ')):
+		case 1:
+			DisplayStack()
+		case 2:
+			num = int(input('Number to add : '))
+			AddToStack(num)
+		case 3:
+			break
--- prac_q13.py
+++ prac_q13.py
@@ -1,14 +1,14 @@
 def stackStr(toProc):
-    Stack = []
-    for i in toProc:
-        if i == "#" and len(Stack) > 0:
-            Stack.pop()
-        else:
-            Stack.append(i)
-    return Stack
+	Stack = []
+	for i in toProc:
+		if i == "#" and len(Stack) > 0:
+			Stack.pop()
+		else:
+			Stack.append(i)
+	return Stack
 
 def AreEqual(str1, str2):
-    return stackStr(str1) == stackStr(str2)
+	return stackStr(str1) == stackStr(str2)
 
 inp1 = input("First string : ")
 inp2 = input("Second string : ")
